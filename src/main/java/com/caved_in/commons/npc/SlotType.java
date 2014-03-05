e28121531cbb6d6c0c66fc85cfb0e8d6444a5de8
Brandon Curtis <brandon@caved.in>
2014-03-05 19:17:33 -0400

    Updated SlotType with getTypeFor methods

--- src/main/java/com/caved_in/commons/npc/SlotType.java
+++ src/main/java/com/caved_in/commons/npc/SlotType.java
@@ -1,14 +1,36 @@
 package com.caved_in.commons.npc;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.bukkit.Material.*;
+
 public enum SlotType {
 
 	ITEM_IN_HAND,
-	HELMET,
-	BODY_ARMOR,
-	PANTS,
-	SHOES;
+	HELMET(LEATHER_HELMET, CHAINMAIL_HELMET, GOLD_HELMET, DIAMOND_HELMET, IRON_HELMET),
+	BODY_ARMOR(LEATHER_CHESTPLATE, CHAINMAIL_CHESTPLATE, GOLD_CHESTPLATE, DIAMOND_CHESTPLATE, IRON_CHESTPLATE),
+	PANTS(LEATHER_LEGGINGS, CHAINMAIL_LEGGINGS, GOLD_LEGGINGS, DIAMOND_LEGGINGS, IRON_LEGGINGS),
+	SHOES(LEATHER_BOOTS, CHAINMAIL_BOOTS, GOLD_BOOTS, DIAMOND_BOOTS, IRON_BOOTS);
 
-	private SlotType() {
+	private static Map<Material, SlotType> materialSlots = new HashMap<>();
+
+	static {
+		for (SlotType slotType : EnumSet.allOf(SlotType.class)) {
+			for (Material material : slotType.materials) {
+				materialSlots.put(material, slotType);
+			}
+		}
+	}
+
+	private Material[] materials;
+
+	SlotType(Material... materials) {
+		this.materials = materials;
 	}
 
 	public int toNumeric() {
@@ -25,6 +47,18 @@ public enum SlotType {
 				return 4;
 			default:
 				return -1;
+		}
+	}
+
+	public static SlotType getTypeFor(ItemStack itemStack) {
+		return getTypeFor(itemStack.getType());
+	}
+
+	public static SlotType getTypeFor(Material material) {
+		if (materialSlots.containsKey(material)) {
+			return materialSlots.get(material);
+		} else {
+			return SlotType.ITEM_IN_HAND;
 		}
 	}
 }
