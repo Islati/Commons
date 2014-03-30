e00c0d400c3d26e417aaf3696a417185182485d8
Brandon <asydix@gmail.com>
2014-03-30 16:14:58 -0300

    Renamed variables to lower camel casing

--- src/main/java/com/caved_in/commons/threading/RunnableManager.java
+++ src/main/java/com/caved_in/commons/threading/RunnableManager.java
@@ -13,42 +13,42 @@ public class RunnableManager {
 		this.plugin = Plugin;
 	}
 
-	public void registerSynchRepeatTask(String Name, Runnable Task, long DelayInTicks, long RepeatTimeInTicks) {
-		if (!runningTasks.containsKey(Name)) {
-			runningTasks.put(Name, this.plugin.getServer().getScheduler().scheduleSyncRepeatingTask(this.plugin, Task, DelayInTicks, RepeatTimeInTicks));
+	public void registerSynchRepeatTask(String name, Runnable task, long delayInTicks, long repeatTimeInTicks) {
+		if (!runningTasks.containsKey(name)) {
+			runningTasks.put(name, plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, task, delayInTicks, repeatTimeInTicks));
 		}
 	}
 
-	public void registerASynchRepeatTask(String Name, Runnable Task, long DelayInTicks, long RepeatTimeInTicks) {
-		if (!runningTasks.containsKey(Name)) {
-			runningTasks.put(Name, this.plugin.getServer().getScheduler().scheduleAsyncRepeatingTask(this.plugin, Task, DelayInTicks, RepeatTimeInTicks));
+	public void registerASynchRepeatTask(String name, Runnable task, long delayInTicks, long repeatTimeInTicks) {
+		if (!runningTasks.containsKey(name)) {
+			runningTasks.put(name, plugin.getServer().getScheduler().scheduleAsyncRepeatingTask(plugin, task, delayInTicks, repeatTimeInTicks));
 		}
 	}
 
-	public void runTaskNow(Runnable Task) {
-		this.plugin.getServer().getScheduler().runTask(this.plugin, Task);
+	public void runTaskNow(Runnable task) {
+		plugin.getServer().getScheduler().runTask(this.plugin, task);
 	}
 
-	public void runTaskAsynch(Runnable Task) {
-		this.plugin.getServer().getScheduler().runTaskAsynchronously(this.plugin, Task);
+	public void runTaskAsynch(Runnable task) {
+		plugin.getServer().getScheduler().runTaskAsynchronously(plugin, task);
 	}
 
-	public void runTaskLater(Runnable Task, long DelayInTicks) {
-		this.plugin.getServer().getScheduler().runTaskLater(this.plugin, Task, DelayInTicks);
+	public void runTaskLater(Runnable task, long delayInTicks) {
+		plugin.getServer().getScheduler().runTaskLater(plugin, task, delayInTicks);
 	}
 
 	public void runTaskOneTickLater(Runnable task) {
-		this.plugin.getServer().getScheduler().runTaskLater(this.plugin, task, 1);
+		plugin.getServer().getScheduler().runTaskLater(plugin, task, 1);
 	}
 
-	public void runTaskLaterAsynch(Runnable Task, long Delay) {
-		this.plugin.getServer().getScheduler().runTaskLaterAsynchronously(this.plugin, Task, Delay);
+	public void runTaskLaterAsynch(Runnable task, long delay) {
+		plugin.getServer().getScheduler().runTaskLaterAsynchronously(plugin, task, delay);
 	}
 
-	public boolean cancelTask(String Name) {
-		if (this.runningTasks.containsKey(Name)) {
-			Bukkit.getScheduler().cancelTask(this.runningTasks.get(Name));
-			this.runningTasks.remove(Name);
+	public boolean cancelTask(String name) {
+		if (runningTasks.containsKey(name)) {
+			Bukkit.getScheduler().cancelTask(this.runningTasks.get(name));
+			this.runningTasks.remove(name);
 			return true;
 		}
 		return false;
