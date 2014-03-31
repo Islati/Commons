df60a76d4b2992335ad9d07a1514eadde2212b9f
Brandon <asydix@gmail.com>
2014-03-31 17:52:53 -0300

    More database migration

--- src/main/java/com/caved_in/commons/sql/DatabaseField.java
+++ src/main/java/com/caved_in/commons/sql/DatabaseField.java
@@ -9,22 +9,48 @@ package com.caved_in.commons.sql;
  * ----------------------------------------------------------------------------
  */
 public enum DatabaseField {
-	/* Fields related for to the player table */
-	PLAYER_UNIQUE_ID("PL_UID"),
-	PLAYER_NAME("PL_NAME"),
-	PLAYER_NAME_PREFIX("PREFIX"),
-	PLAYER_IP_ADDRESS("PL_IP"),
-	/* Online status field for the online-status */
-	ONLINE("ONLINE"),
-	/* Fields related to the shop table(s) */
-	SHOP_ITEM_ID("ITEM_ID"),
-	SHOP_ITEM_NAME("ITEM_NAME"),
-	SHOP_TIME_PURCHASED("TIME_PURCHASE"),
-	SHOP_ITEM_PRICE("ITEM_PRICE"),
-	SHOP_ITEM_DESCRIPTION("ITEM_DESCRIPTION"),
-	SHOP_ITEM_PURCHASE_TIME("")
+	//Fields related to the player table
+	PLAYER_UNIQUE_ID("player_id"),
+	PLAYER_NAME("player_name"),
+	PLAYER_NAME_PREFIX("player_prefix"),
+	PLAYER_IP_ADDRESS("player_ip"),
+	//Fields related to the premium table
+	PREMIUM_STATUS("premium"),
 
-	SERVER_ID("SVR_ID"),
+	// Online status field for the online-status
+	ONLINE_STATUS("online"),
+	//Fields related to the shop table(s)
+	SHOP_ITEM_ID("item_id"),
+	SHOP_ITEM_NAME("item_name"),
+	SHOP_TIME_PURCHASED("time_purchased"),
+	SHOP_ITEM_PRICE("item_price"),
+	SHOP_ITEM_DESCRIPTION("item_desc"),
+	SHOP_ITEM_PURCHASE_TIME("TIME_PURCHASED"),
+	SHOP_ITEM_EXPIRE_TIME("TIME_EXPIRE"),
+	//Fields relating to the server table
+	SERVER_ID("svr_id"),
+	SERVER_NAME("svr_name"),
+	SERVER_IP("svr_ip"),
+	SERVER_PLAYER_LIMIT("svr_player_limit"),
+	//Punishments and related table data
+	PUNISHMENT_ID("PUN_ID"),
+	PUNISHMENT_ISSUER_UID("PUN_ISSUER_UID"),
+	PUNISHMENT_ISSUED_TIME("PUN_ISSUED"),
+	PUNISHMENT_EXPIRATION_TIME("PUN_EXPIRE"),
+	PUNISHMENT_REASON("PUN_REASON"),
+	//Friends Table
+	FRIEND_USER_ID
 
 
+
+	private String columnName;
+
+	DatabaseField(String columnName) {
+		this.columnName = columnName;
 	}
+
+	@Override
+	public String toString() {
+		return columnName;
+	}
+}
